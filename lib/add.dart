ad20288b6abbd61618de1b25cfb6e106eed7dde5
MUHAMMAD TARMIZI SANUSI AFKAR <realtarmizisanusi@gmail.com>
2020-02-16 10:24:38 +0800

    use datepicker plugins

--- lib/add.dart
+++ lib/add.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:datetime_picker_formfield/datetime_picker_formfield.dart';
+import 'package:intl/intl.dart';
 
 class AddPage extends StatefulWidget {
   @override
@@ -8,44 +10,56 @@ class AddPage extends StatefulWidget {
 class _AddPageState extends State<AddPage> {
   final namecontroller = TextEditingController();
   final descriptioncontroller = TextEditingController();
-  final datecontroller = TextEditingController();
+  // final datecontroller = TextEditingController();
+  final format = DateFormat("yyyy-MM-dd");
+  String dateSelected;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Add New Item")
-      ),
+      appBar: AppBar(title: Text("Add New Item")),
       body: Container(
-        child: Column(children: <Widget>[
+          child: Column(
+        children: <Widget>[
           TextField(
-            decoration: InputDecoration(
-              hintText: "Enter item name"
-            ),
+            decoration: InputDecoration(hintText: "Enter item name"),
             controller: namecontroller,
           ),
           TextField(
-            decoration: InputDecoration(
-              hintText: "Enter item description"
-            ),
+            decoration: InputDecoration(hintText: "Enter item description"),
             controller: descriptioncontroller,
           ),
-          TextField(
-            keyboardType: TextInputType.datetime,
-            decoration: InputDecoration(
-              hintText: "Enter item date"
-            ),
-            controller: datecontroller,
+          // TextField(
+          //   keyboardType: TextInputType.datetime,
+          //   decoration: InputDecoration(
+          //     hintText: "Enter item date"
+          //   ),
+          //   controller: datecontroller,
+          // ),
+          DateTimeField(
+            format: format,
+            onChanged: (dt) {
+              setState(() {
+                dateSelected = "${dt.day}/${dt.month}/${dt.year}";
+              });
+            },
+            onShowPicker: (context, currentValue) {
+              return showDatePicker(
+                  context: context,
+                  firstDate: DateTime(1900),
+                  initialDate: currentValue ?? DateTime.now(),
+                  lastDate: DateTime(2100));
+            },
           ),
           FlatButton(
-            onPressed: (){
-              //Navigator.pop(context);
-              print(" Name ${namecontroller.text} Description ${descriptioncontroller.text} Date ${datecontroller.text}");
-            }, 
-            child: Text("Save")
-          )
-        ],)
-      ),
-      
+              onPressed: () {
+                //Navigator.pop(context);
+                print(
+                    " Name ${namecontroller.text} Description ${descriptioncontroller.text} Date $dateSelected");
+              },
+              child: Text("Save"))
+        ],
+      )),
     );
   }
 }
