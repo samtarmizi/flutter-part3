2607805a8b3bb83500b81782909843003a016b1d
MUHAMMAD TARMIZI SANUSI AFKAR <realtarmizisanusi@gmail.com>
2020-02-16 11:26:40 +0800

    Use Card and Center

--- lib/detail.dart
+++ lib/detail.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/material.dart';
 
-
 class DetailPage extends StatefulWidget {
   final String name;
   final String description;
   final String date;
 
   const DetailPage(this.name, this.description, this.date);
-  
+
   @override
   _DetailPageState createState() => _DetailPageState();
 }
@@ -16,17 +15,19 @@ class _DetailPageState extends State<DetailPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Detail Page")
-      ),
+      appBar: AppBar(title: Text("Detail Page")),
       body: Container(
-        child: Column(children: <Widget>[
-          Text('${widget.name}'),
-          Text('${widget.description}'),
+          child: Card(
+              child: Center(
+                  child: Column(
+        children: <Widget>[
+          Text('${widget.name}',style: TextStyle(fontSize: 32, color: Colors.grey[450])),
+          SizedBox(height: 8),
+          Text('${widget.description}', style: TextStyle(fontSize: 15, color: Colors.grey[450]),),
+          SizedBox(height: 8),
           Text('${widget.date}')
-        ],)
-      ),
-      
+        ],
+      )))),
     );
   }
 }
