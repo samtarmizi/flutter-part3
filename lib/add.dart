ba5989440958ceba8dd7ac36c4da9c8556dc9db7
MUHAMMAD TARMIZI SANUSI AFKAR <realtarmizisanusi@gmail.com>
2020-02-16 09:43:26 +0800

    Create input page to add

---
+++ lib/add.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class AddPage extends StatefulWidget {
+  @override
+  _AddPageState createState() => _AddPageState();
+}
+
+class _AddPageState extends State<AddPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Add New Item")
+      ),
+      body: Container(
+        child: Column(children: <Widget>[
+          TextField(
+            decoration: InputDecoration(
+              hintText: "Enter item name"
+            ),
+          ),
+          TextField(
+            decoration: InputDecoration(
+              hintText: "Enter item description"
+            ),
+          ),
+          TextField(
+            decoration: InputDecoration(
+              hintText: "Enter item place"
+            ),
+          ),
+        ],)
+      ),
+      
+    );
+  }
+}
